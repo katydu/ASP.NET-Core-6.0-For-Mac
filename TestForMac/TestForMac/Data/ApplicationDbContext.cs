641f175be98f89c6c27661ed4690d099e51d81c6
Katy <surekaty0919@gmail.com>
2022-10-23 13:33:58 +0800

    first commit

---
+++ TestForMac/TestForMac/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using TestForMac.Models;
+
+namespace TestForMac.Data;
+
+public class ApplicationDbContext : DbContext
+{
+	// basic config for connect the entity framework
+	public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+	{
+
+	}
+
+	public DbSet<Category> Categories { get; set; }
+}
+
---
+++ TestForMac/TestForMac/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TestForMac.Models;
+
+public class Category
+{
+	[Key]
+	public int Id { get; set; }
+	[Required]
+	public string Name { get; set; }
+	public int DisplayDateTime { get; set; }
+	public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+}
+
