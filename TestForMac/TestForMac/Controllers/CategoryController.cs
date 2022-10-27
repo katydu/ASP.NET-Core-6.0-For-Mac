f2e376ed20ef054b5704195df961528f7cf6966b
Katy <surekaty0919@gmail.com>
2022-10-27 20:10:46 +0800

    finish all functions

--- TestForMac/TestForMac/Controllers/CategoryController.cs
+++ TestForMac/TestForMac/Controllers/CategoryController.cs
@@ -23,7 +23,7 @@ namespace TestForMac.Controllers
             _db = db;
         }
 
-        // GET: /<controller>/
+        [HttpGet]
         public IActionResult Index()
         {
             IEnumerable<Category> objCategoryList = _db.Categories;
@@ -31,9 +31,104 @@ namespace TestForMac.Controllers
             return View(objCategoryList);
         }
 
+        /// <summary>
+        /// delete data
+        /// </summary>
+        /// <param name="id">category id</param>
+        /// <returns></returns>
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Index(int id)
+        {
+            var obj = _db.Categories.Find(id);
+            if (obj == null)
+            {
+                NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category delete successfully";
+            return RedirectToAction("Index","Category");
+        }
+
+        [HttpGet]
         public IActionResult Create()
         {
             return View();
+        }
+
+        /// <summary>
+        /// add new category
+        /// </summary>
+        /// <param name="obj">name and displayOrder</param>
+        /// <returns></returns>
+        // ValidateAntiForgeryToken : prevent CSRF(Cross-Site Request Forgery) attack
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if(obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "The DisplayOrder cannot exactly match the Name");
+            }
+            // server side validation 
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Add Category successfully";
+                return RedirectToAction("Index");
+                //return RedirectToAction("Action","Controller");
+            }
+            return View(obj);
+        }
+
+        /// <summary>
+        /// enter edit page
+        /// </summary>
+        /// <param name="id">category id</param>
+        /// <returns>edit page</returns>
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+            //var categoryFromDbFirst = _db.Categories.FirstOrDefault(u => u.Id == id);
+            //var categoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id);
+
+            if(categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        /// <summary>
+        /// add edit into db
+        /// </summary>
+        /// <param name="obj">new change</param>
+        /// <returns></returns>
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "The DisplayOrder cannot exactly match the Name");
+            }
+            // server side validation 
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Edit category successfully";
+                return RedirectToAction("Index");
+                //return RedirectToAction("Action","Controller");
+            }
+            return View(obj);
         }
     }
 }
